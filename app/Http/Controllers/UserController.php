48ab80d560839987c180fda59033e185de527e16
Abubakarbaiggg <Abubakar192005@gmail.com>
2025-10-15 01:49:49 +0500

    User Controller.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,18 +13,28 @@ class UserController extends Controller
      */
     public function index()
     {
+        // Get all users who have placed more than 5 completed orders.
+
         // ======================= SELECT QUERY =============================
         $mySqlQuery = "SELECT u.name,COUNT(o.id) as total_order FROM users u  JOIN orders o ON u.id = o.user_id  WHERE o.status = 'completed'
-                GROUP BY u.id,u.name HAVING total_order >= 9";
-        // ======================= EQUOLENT QUERY =============================
-        $equolentQuery1 = User::with('order as o')->select('name',DB::raw('COUNT(orders.id) as total_order'))->
-        groupBy('users.id','users.name')->having('total_order','>=',7)->get();
-        dd($equolentQuery1);
-        // ======================= RESULT QUERY =============================
+                GROUP BY u.id,u.name HAVING total_order >= 7";
         $data = DB::select($mySqlQuery);
         foreach ($data as $result) {
             echo "Name : $result->name <br>";
             echo "Total Order : $result->total_order";
+            echo '<hr>';
+        }
+        // ======================= EQUOLENT QUERY =============================
+        $equolentQuery1 = User::withCount(['order as total_order' => function ($query) {
+            $query->where('status', 'completed');
+        }])->having('total_order', '>=', 7)->get()
+            ->map(fn ($data) => [
+                'name' => $data->name,
+                'totalOrders' => $data->total_order,
+            ]);
+        foreach ($equolentQuery1 as $data) {
+            echo 'Name: '.$data['name'].'<br>'.
+                 'Password: '.$data['totalOrders'].'<hr>';
         }
     }
 
