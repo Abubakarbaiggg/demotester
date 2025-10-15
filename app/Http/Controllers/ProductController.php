a8ff22e73d2a6de5d0141a906cd9ca8edcd7577f
Abubakar-telecard <abubakar_baig@telecard.com.pk>
2025-10-15 17:27:00 +0500

    Product Query.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -12,7 +13,25 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        // ================== Find the top 3 products with the highest total quantity ordered.  ===================
+
+        echo "======================= MYSQL QUERY ============================== <br>";
+
+        $product = "SELECT p.name, COUNT(oi.id) as highest_order FROM products p JOIN order_items oi ON oi.product_id = p.id GROUP BY p.name
+                    ORDER BY highest_order DESC LIMIT 3";
+        $data1 = DB::select($product);
+        foreach ($data1 as $data) {
+            echo 'Name: ' . $data->name . '<br>' . 'Password: ' . $data->highest_order . '<hr>';
+        }
+
+        echo "======================= EQUOLENT QUERY ============================= <br>";
+
+        echo "========================= QUERY ONE ================================ <br>";
+
+        // $product = Product::with('orderItem')->get()->toArray();
+        $product = Product::with('orderItem')->select(['name','orders.product_id','orders.order_id'])->get()->toArray();
+        dd($product);
+
     }
 
     /**
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,29 +13,51 @@ class UserController extends Controller
      */
     public function index()
     {
-        // Get all users who have placed more than 5 completed orders.
+              /*// Get all users who have placed more than 5 completed orders.
 
-        // ======================= SELECT QUERY =============================
-        $mySqlQuery = "SELECT u.name,COUNT(o.id) as total_order FROM users u  JOIN orders o ON u.id = o.user_id  WHERE o.status = 'completed'
-                GROUP BY u.id,u.name HAVING total_order >= 7";
-        $data = DB::select($mySqlQuery);
-        foreach ($data as $result) {
-            echo "Name : $result->name <br>";
-            echo "Total Order : $result->total_order";
-            echo '<hr>';
-        }
-        // ======================= EQUOLENT QUERY =============================
-        $equolentQuery1 = User::withCount(['order as total_order' => function ($query) {
-            $query->where('status', 'completed');
-        }])->having('total_order', '>=', 7)->get()
-            ->map(fn ($data) => [
-                'name' => $data->name,
-                'totalOrders' => $data->total_order,
-            ]);
-        foreach ($equolentQuery1 as $data) {
-            echo 'Name: '.$data['name'].'<br>'.
-                 'Password: '.$data['totalOrders'].'<hr>';
-        }
+              echo "======================= MYSQL QUERY ============================== <br>";
+
+              $mySqlQuery = "SELECT u.name,COUNT(o.id) as total_order FROM users u  JOIN orders o ON u.id = o.user_id  
+              WHERE o.status = 'completed' GROUP BY u.id,u.name HAVING total_order >= 7";
+              $data = DB::select($mySqlQuery);
+              foreach ($data as $result) {
+                  echo "Name : $result->name <br>";
+                  echo "Total Order : $result->total_order";
+                  echo '<hr>';
+              }
+
+              echo "======================= EQUOLENT QUERY ============================= <br>";
+
+              echo "========================= QUERY ONE ================================ <br>";
+
+              $equolentQuery1 = User::withCount(['order as total_order' => function ($query) {
+                  $query->where('status', 'completed');
+              }])->having('total_order', '>=', 7)->get()
+                  ->map(fn ($data) => [
+                      'name' => $data->name,
+                      'total_order' => $data->total_order,
+                  ]);
+              foreach ($equolentQuery1 as $data) {
+                  echo 'Name: '.$data['name'].'<br>'. 'Password: '.$data['total_order'].'<hr>';
+              }
+
+              echo "========================= QUERY TWO ================================ <br>";
+
+              $equolentQuery2 = User::join('orders as ord','ord.user_id','users.id')
+              ->select("users.name",DB::raw("COUNT(ord.user_id) as total_order"))->where('status','=','Completed')
+              ->groupBy('users.name')->get()->toArray();
+              foreach ($equolentQuery2 as $data) {
+                  echo 'Name: '.$data['name'].'<br>'. 'Password: '.$data['total_order'].'<hr>';
+              }
+
+              echo "========================= ROW QUERY ================================ <br>";
+
+              $rowQuery = DB::table('users as u')->join('orders as o','o.user_id','u.id')
+              ->selectRaw("u.name,COUNT(o.user_id) as total_order")->whereRaw("o.status = 'Completed'")
+              ->groupBy('u.name')->get();
+              foreach ($rowQuery as $data) {
+                  echo 'Name: '.$data->name.'<br>'. 'Password: '.$data->total_order.'<hr>';
+              }*/
     }
 
     /**
